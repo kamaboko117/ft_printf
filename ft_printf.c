fbb2c24e8191c877bde5cc9be032c8717b914fc6
Alexander SABOURET <asaboure@e1r10p13.42.fr>
2020-02-04 18:18:11 +0100

    printf continued, putnbr modified so there's no exception with int min

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,66 @@
 /*   By: asaboure <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/29 15:54:47 by asaboure          #+#    #+#             */
-/*   Updated: 2020/01/30 18:35:51 by asaboure         ###   ########.fr       */
+/*   Updated: 2020/02/04 17:45:14 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stdio.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include "libft/libft.h"
 
-int	find_index(char *tab, char c)
+int		find_index(char *tab, char c)
 {
 	int	i;
 
 	i = 0;
-	while (tab[i++])
+	while (tab[i])
 	{
 		if (tab[i] == c)
 			return (i);
+		i++;
 	}
 	return (-1);
 }
 
-int	ft_printf(const char *format, ...)
+void	chr(va_list *list)
 {
-	void	(*functions[3])(va_list *) = {str, nbr, chr}
-	char	flags[4] = {'s', 'd', 'c', 0};
+	int chr;
+	
+	chr = va_arg(*list, int);
+	ft_putchar_fd(chr, 1);
+}
+
+void	str(va_list *list)
+{
+	char	*str;
+
+	str = va_arg(*list, char *);
+	ft_putstr_fd(str, 1);
+}
+
+void	hex(va_list *list)
+{
+	while (list);
+}
+
+void	nbr(va_list *list)
+{
+	int	nbr;
+
+	nbr = va_arg(*list, int);
+	ft_putnbr_fd(nbr, 1);
+}
+
+void	nbrhex(va_list *list)
+{
+	while (list);
+}
+
+int		ft_printf(const char *format, ...)
+{
+	void	(*functions[8])(va_list *) = {chr, str, hex, nbr, nbr, nbr, nbrhex, nbrhex};
+	char	flags[9] = {'c', 's', 'p', 'd', 'i', 'u', 'x', 'X', 0};
 	va_list	list;
 	int		i;
 	int		tmp;
@@ -39,13 +75,15 @@ int	ft_printf(const char *format, ...)
 	va_start(list, format);
 	while (format[i++])
 	{
-		if (i != 0 && format[i - 1] == '%')
+		if (i != 0 && format[i - 1] == '%' && format[i] != '%')
 		{
 			tmp = find_index(flags, format[i]);
+		//	printf("index: %d\n", tmp);
 			if (tmp != -1)
 				(*functions[tmp])(&list);
 		}
 		else if (format[i] != '%')
 			write(1, &format[i], 1);
 	}
+	return (1);
 }
--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,35 +6,36 @@
 /*   By: asaboure <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/12 16:04:09 by asaboure          #+#    #+#             */
-/*   Updated: 2019/11/23 16:34:56 by asaboure         ###   ########.fr       */
+/*   Updated: 2020/02/04 18:13:49 by asaboure         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <unistd.h>
 
-void	ft_putnbr_fd(int n, int fd)
+void	ft_recurr(int n, int fd)
 {
 	char a;
 
-	if (n == -2147483648)
-		write(fd, "-2147483648", 11);
+	if (n <= -10)
+	{
+		ft_recurr(n / 10, fd);
+		ft_recurr(n % 10, fd);
+	}
 	else
 	{
+		a = -n + 48;
+		write(fd, &a, 1);
+	}
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	{
 		if (n < 0)
-		{
-			n = -n;
 			write(fd, "-", 1);
-		}
-		if (n >= 10)
-		{
-			ft_putnbr_fd(n / 10, fd);
-			ft_putnbr_fd(n % 10, fd);
-		}
 		else
-		{
-			a = n + 48;
-			write(fd, &a, 1);
-		}
+			n = -n;
+		ft_recurr(n, fd);
 	}
 }
